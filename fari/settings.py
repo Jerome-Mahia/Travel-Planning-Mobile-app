e9aa067485d66c3514ad837e556cba914f5bae26
MIKKE-WANGO <mikemundati@gmail.com>
2023-06-20 12:59:57 +0300

    settigs

--- fari/settings.py
+++ fari/settings.py
@@ -164,7 +164,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mikemundati@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 

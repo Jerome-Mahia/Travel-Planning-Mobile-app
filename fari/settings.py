541df387468e1ce9a023aed658cc5d358135c751
MIKKE-WANGO <mikemundati@gmail.com>
2023-11-13 15:15:01 +0300

    db

--- fari/settings.py
+++ fari/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
 DATABASES = {
    'default': dj_database_url.config(
     #Feel free to alter this value to suit your needs.
-    default='postgres://fari_db_361r_user:LITbyeIzst9hNltwFcWv6DvLrEA3WAwF@dpg-cl90v4v6e7vc73abvntg-a/fari_db_361r',
+    default='postgres://fari_db_ywcc_user:WOV6QTT5cefka9tIXD049CjPki95gUu8@dpg-cl915o2vokcc73b53rvg-a/fari_db_ywcc',
     conn_max_age=600
   )
 }
